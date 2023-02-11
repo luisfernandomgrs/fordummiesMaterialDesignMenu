78d51f6eb9caa2a845e5c94a2af4cdd4b2e4d08c
Luis Fernando Machado <>
2023-02-10 23:45:35 -0300

    personalized toolbar, added to main activity

--- app/src/main/java/com/materialdesign/menu/MainActivity.java
+++ app/src/main/java/com/materialdesign/menu/MainActivity.java
@@ -1,14 +1,20 @@
 package com.materialdesign.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+	private Toolbar toolbar;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		toolbar = (Toolbar) findViewById(R.id.toolbar_personal);
+		setSupportActionBar(toolbar);
 	}
 }
