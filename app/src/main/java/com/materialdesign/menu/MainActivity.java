463fc955dddd2331d5f752a6a8e1e48d4f18875c
Luis Fernando Machado <>
2023-02-10 23:59:12 -0300

    Events click... onOptionsItemSelected

--- app/src/main/java/com/materialdesign/menu/MainActivity.java
+++ app/src/main/java/com/materialdesign/menu/MainActivity.java
@@ -1,10 +1,13 @@
 package com.materialdesign.menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
 		//return super.onCreateOptionsMenu(menu);
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+		switch (item.getItemId()) {
+			case R.id.menu_search: {
+				Toast.makeText(this, "Seach menu item, selected", Toast.LENGTH_SHORT).show();
+				break;
+			}
+			case R.id.menu_cart: {
+				Toast.makeText(this, "Cart menu item, selected", Toast.LENGTH_SHORT).show();
+				break;
+			}
+			case R.id.menu_profile: {
+				Toast.makeText(this, "Profile menu item, selected", Toast.LENGTH_SHORT).show();
+				break;
+			}
+			case R.id.menu_settings: {
+				Toast.makeText(this, "Settings menu item, selected", Toast.LENGTH_SHORT).show();
+				break;
+			}
+			case R.id.menu_about: {
+				Toast.makeText(this, "About menu item=, selected", Toast.LENGTH_SHORT).show();
+				break;
+			}
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
